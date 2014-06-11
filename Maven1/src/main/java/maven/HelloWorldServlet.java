031bb42b485caf2a0469948aafb8235f940a3a28
Nagendra(NK) <Nagendra(NK)@NagendraNK-PC.Home>
2014-06-11 22:02:50 +0530

    commit 2

--- Maven1/src/main/java/maven/HelloWorldServlet.java
+++ Maven1/src/main/java/maven/HelloWorldServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
  
  
-public class HelloWorldServlet extends HttpServlet {
+public class HelloWorldServlet extends HttpServlet{
      
     private static final long serialVersionUID = 1031422249396784970L;
  
